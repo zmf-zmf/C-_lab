22557fca290c090a1388b1b1d6c246cef11d9444
Mengfan <18317437831@163.com>
2025-10-19 23:10:04 +0800

    ‘test: Pass initial and default pose test cases

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -5,7 +5,7 @@ Executor* Executor::NewExecutor(const Pose& pose) noexcept
 {
     return new (std::nothrow) ExecutorImpl(pose);
 }
-ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept
+ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 {
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
