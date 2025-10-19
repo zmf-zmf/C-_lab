13dd0e36464bbe29819d0df95a1925ea4b81a9ad
Mengfan <18317437831@163.com>
2025-10-19 23:38:44 +0800

    test:Pass initial and default pose test cases

--- cpp-training/tests/ExecutorTest.cpp
+++ cpp-training/tests/ExecutorTest.cpp
@@ -30,4 +30,21 @@ TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
     const Pose target({0, 0, 'N'});
     ASSERT_EQ(target, executor->Query());
 }
+TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("M");
+    // then
+    const Pose target({1, 0, 'E'});
+    ASSERT_EQ(target, executor->Query());
 }
+TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_W)
+{
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+    executor->Execute("M");
+    const Pose target({-1, 0, 'W'});
+    ASSERT_EQ(target, executor->Query());
+}
+}  // namespace adas
