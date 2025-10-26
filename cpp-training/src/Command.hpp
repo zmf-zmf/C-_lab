671d8b9794c25094bec1359e31ab9c9c78589788
Mengfan <18317437831@163.com>
2025-10-26 17:15:11 +0800

    Commande.hpp

---
+++ cpp-training/src/Command.hpp
@@ -0,0 +1,97 @@
+#pragma once
+#include "ExecutorImpl.hpp"
+namespace adas
+{
+class ICommand
+{
+public:
+    virtual ~ICommand() = default;
+    virtual void DoOperate(ExecutorImpl& executor) const noexcept = 0;
+};
+
+private:
+bool IsFast(void) const noexcept;
+void Fast(void) noexcept;
+bool fast{false};
+
+private:
+Pose pose;
+class ICommand
+{
+public:
+    virtual ~ICommand() = default;
+    virtual void DoOperate(ExecutorImpl& executor) const noexcept = 0;
+};
+class FastCommand final : public ICommand
+{
+public:
+    void DoOperate(ExecutorImpl& executor) const noexcept override
+    {
+        executor.Fast();
+    }
+};
+class MoveCommand final : public ICommand
+{
+public:
+    void DoOperate(ExecutorImpl& executor) const noexcept override
+    {
+        if (executor.IsFast()) {
+            executor.Move();
+        }
+        executor.Move();
+    }
+};
+class TurnLeftCommand final : public ICommand
+{
+public:
+    void DoOperate(ExecutorImpl& executor) const noexcept override
+
+    {
+        if (executor.IsFast()) {
+            executor.Move();
+        }
+        executor.TurnLeft();
+    }
+};
+class TurnRightCommand final : public ICommand
+{
+public:
+    void DoOperate(ExecutorImpl& executor) const noexcept override
+
+    {
+        if (executor.IsFast()) {
+            executor.Move();
+        }
+        executor.TurnRight();
+    }
+};
+class BackUpCommand final : public ICommand
+{
+public:
+    void DoOperate(ExecutorImpl& executor) const noexcept override
+
+    {
+        if (executor.IsFast()) {
+            executor.BackUp();
+        }
+        executor.BackUp();
+    }
+};
+class UTurnCommand final : public ICommand
+{
+public:
+    void DoOperate(ExecutorImpl& executor) const noexcept override
+
+    {
+        if (executor.IsFast()) {
+            executor.Move();
+        }
+        executor.UTurn();
+    }
+};
+void Move(void) noexcept;
+void TurnLeft(void) noexcept;
+void TurnRight(void) noexcept;
+void BackUp(void) noexcept;
+void UTurn(void) noexcept;
+};  // namespace adas
