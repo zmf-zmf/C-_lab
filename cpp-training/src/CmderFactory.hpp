c3cf7e5fcf5bbd85ae519e7a856644cb36bbd35b
Mengfan <18317437831@163.com>
2025-11-09 23:15:32 +0800

    引入Command层

--- cpp-training/src/CmderFactory.hpp
+++ cpp-training/src/CmderFactory.hpp
@@ -4,24 +4,26 @@
 #include <unordered_map>
 
 #include "Command.hpp"
-
 namespace adas
 {
+using Cmder = std::function<void(PoseHandler& poseHandler)>;
+using CmderList = std::list<Cmder>;
+
 class CmderFactory final
 {
 public:
     CmderFactory(void) noexcept = default;
+    ~CmderFactory(void) noexcept = default;
+
     CmderFactory(const CmderFactory&) noexcept = delete;
     CmderFactory& operator=(const CmderFactory&) noexcept = delete;
 
 public:
-    std::list<std::function<void(PoseHandler& poseHandler)>> Cmders(const std::string& commands) const noexcept;
+    CmderList GetCmders(const std::string& commands) const noexcept;
 
 private:
     const std::unordered_map<char, std::function<void(PoseHandler& poseHandler)>> cmderMap{
         {'M', MoveCommand()}, {'L', TurnLeftCommand()}, {'R', TurnRightCommand()},
-        {'F', FastCommand()}, {'B', ReverseCommand()},
-    };
+        {'F', FastCommand()}, {'B', ReverseCommand()},  {'U', UTurnCommand()}};
 };
-
 }  // namespace adas
